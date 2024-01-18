876504ea0cbc20e23f99af92834314745eecc145
asosiymenu007 <asosiymenu007@gmail.com>
2024-01-19 01:14:36 +0500

    uyga vazifa

---
+++ script.js
@@ -0,0 +1,45 @@
+// 1-Misol
+
+// let num = 4726;
+// let counter = 0;
+// // let m = (num - num % 1000) / 1000
+// let m = Math.trunc(num / 1000);
+// let y = Math.trunc((num / 100) % 10);
+// let o = Math.trunc((num / 10) % 10);
+// let b = num % 10;
+// if (m % 2 == 0) {
+//     counter += 1
+// }
+// if (y % 2 == 0) {
+//     counter += 1
+// }
+// if (o % 2 == 0) {
+//     counter += 1
+// }
+// if (b % 2 == 0) {
+//     counter += 1
+// }
+// console.log(counter);
+
+
+// 2-Misol
+
+// let son = 532;
+// let y = Math.trunc(son / 100);
+// let o = Math.trunc((son / 10) % 10);
+// let b = son % 10;
+// let sum = y + o + b;
+// let res = `false`
+// res = sum % 2 == 0 && sum % 5 == 0 ? `true` : `false`
+// console.log(res);
+
+
+
+/**
+ * 3-Misol
+*/
+// let son1 = 33;
+// let son2 = 31;
+// let son3 = 34;
+// let res = Math.trunc((son1 + son2 + son3) / 3);
+// console.log(res);
